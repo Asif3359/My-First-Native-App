e48357a106355ae238dcb4e9cee82b1dfde075e2
Asif359 <asifahammednishst@gmail.com>
2023-10-23 13:03:33 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import{
+  View,
+  SafeAreaView,
+  Text,
+} from "react-native";
+
+const App =()=>{
+  return(
+    <SafeAreaView>
+      <View>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+        <Text>Hell World</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+export default App;
