e90d3c41baaf127095716a94603db4226119f742
Asif359 <asifahammednishst@gmail.com>
2023-10-23 13:15:20 +0600

    use props poperty

--- App.tsx
+++ App.tsx
@@ -6,6 +6,13 @@ import{
   Text,
 } from "react-native";
 
+const Person =({name})=>{
+
+  return(
+    <Text>{name}</Text>
+  )
+}
+
 const App =()=>{
   return(
     <SafeAreaView>
@@ -20,6 +27,12 @@ const App =()=>{
         <Text>Hell World</Text>
         <Text>Hell World</Text>
         <Text>Hell World</Text>
+        <Text>Hi My Name Is Asif</Text>
+        <Text>Hi My Name Is Asif</Text>
+        {/* Use Props Property */}
+        <Text>Hell <Person name='Asif'></Person></Text>
+        <Text>Hell <Person name='Atik'></Person></Text>
+        <Text>Hell <Person name='Alin'></Person></Text>
       </View>
     </SafeAreaView>
   )
