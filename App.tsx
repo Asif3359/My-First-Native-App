7d27e7ac46576e7e295fb007e2b73d9a91ff13d3
Asif359 <asifahammednishst@gmail.com>
2023-10-24 15:19:19 +0600

    creat simple calculator

--- App.tsx
+++ App.tsx
@@ -6,31 +6,47 @@ import {
   Text,
   TouchableOpacity,
   Alert,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from "react-native";
 type GreetingProps = {
   name: string;
   // count: number;
 };
 
-const styles =StyleSheet.create({
-  screenSize:{
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center"
+const styles = StyleSheet.create({
+  screenSize: {
+    overflow: "scroll",
   },
-  button:{
-    backgroundColor:"gray",
-    textAlign:"center",
-    marginTop:5,
-    padding:3,
-    width:150,
+  inputArea: {
+    marginLeft: 10,
   },
-  titles:{
-    backgroundColor:"green",
-    color:"white"
-    
-  }
+  titles: {
+    backgroundColor: "green",
+    color: "white"
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  display: {
+    fontSize: 24,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    width: 75,
+    height: 75,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 24,
+    margin: 5,
+  },
+
 })
 
 
@@ -44,36 +60,79 @@ const Person = (props: GreetingProps) => {
 const App = () => {
 
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleInput = (value) => {
+    if (value === '=') {
+      try {
+        const calculatedResult = eval(input);
+        setResult(calculatedResult.toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput((prevInput) => prevInput + value);
+    }
+  };
+
+  const buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    'C', '0', '=', '+',
+  ];
 
   const handlePress = () => {
     setCount(count + 1);
-    Alert.alert(`you Click ${count+1} Time`);
+    Alert.alert(`you Click ${count + 1} Time`);
   }
 
   return (
     <SafeAreaView>
       <View style={styles.screenSize}>
-        <Text style={styles.titles}>Hell World</Text>
-  
-        <Text>Hi My Name Is Asif</Text>
-        <Text>Hi My Name Is Asif</Text>
+        <ScrollView>
+          <Text style={styles.titles}>Hell World</Text>
 
-        {/* Use Props Property */}
-        <Text>Hell <Person name='Asif'></Person></Text>
-        <Text>Hell <Person name='Atik'></Person></Text>
-        <Text>Hell <Person name='Alin'></Person></Text>
+          <Text>Hi My Name Is Asif</Text>
+          <Text>Hi My Name Is Asif</Text>
 
-        {/* Using Type script */}
-        <Text>Hell <Person name='Asif Ahammed'></Person></Text>
-        <Text>Click Time: {count}</Text>
-        <TouchableOpacity>
-          <View>
-            <Text onPress={() => handlePress()} style={styles.button} >Click Me</Text>
+          {/* Use Props Property */}
+          <Text>Hell <Person name='Asif'></Person></Text>
+          <Text>Hell <Person name='Atik'></Person></Text>
+          <Text>Hell <Person name='Alin'></Person></Text>
+
+          {/* Using Type script */}
+          <Text>Hell <Person name='Asif Ahammed'></Person></Text>
+          <Text>Click Time: {count}</Text>
+          <TouchableOpacity>
+            <View>
+              <Text onPress={() => handlePress()} style={styles.button} >Click Me</Text>
+              {/* <Text onPress={() => handlePress()} style={styles.button} >Click Me</Text> */}
+            </View>
+          </TouchableOpacity>
+          {/* Try to creat Native Calculator  */}
+
+          <View style={styles.container}>
+            <Text style={styles.display}>{input}</Text>
+            <Text style={styles.display}>{result}</Text>
+            <View style={styles.grid}>
+              {buttons.map((button) => (
+                <Text
+                  key={button}
+                  onPress={() => handleInput(button)}
+                  style={styles.button}
+                >
+                  {button}
+                </Text>
+              ))}
+            </View>
           </View>
-        </TouchableOpacity>
 
-
-
+        </ScrollView>
       </View>
     </SafeAreaView>
   )
